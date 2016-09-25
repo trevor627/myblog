7d2fb340454ca0b0d4d7a069dddb9b7869ffaf20
Trevor <615281792@qq.com>
2016-09-25 23:11:15 +0800

    个人博客

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"beeblog/controllers"
+	"beeblog/models"
+	_ "beeblog/routers"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+)
+
+func init() {
+	models.RegisterDB()
+}
+
+func main() {
+	orm.Debug = true
+	orm.RunSyncdb("default", false, true)
+
+	beego.Router("/", &controllers.MainController{})
+	beego.Router("/login.go", &controllers.LoginController{})
+
+	beego.Run()
+}
---
+++ views/Header.tpl
@@ -0,0 +1,9 @@
+{{define "header"}}
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; chareset-utf-8">
+		<link rel="shortcut icon" href="/static/img/favicon.png">
+		<link rel="stylesheet" type="text/css" href="/static/css/bootstrap.min.css">
+{{end}}
